b3c8cedd572d8ddcb26f8e70c61d167f832102e9
Saurav <sp.gharti@gmail.com>
2019-01-11 17:20:07 +0000

    initial commit

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: waitress-serve --port=$PORT elmedina1crowdboticsc_641.wsgi:application
---
+++ variables.tf
@@ -0,0 +1,12 @@
+
+variable "app_name" {
+  description = "Unique name of the app"
+  type = "string"
+  default = "elmedina1crowdboticsc-641"
+}
+
+variable "custom_domain" {
+  description = "Custom domain name (optional)"
+  type = "string"
+  default = ""
+}
